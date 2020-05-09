5c14d62c1c6a66d8e47124e4b5fcda40667ee7ba
ravi singh <raviranjan.rs44@gmail.com>
2020-05-09 20:48:09 +0530

    commit

--- src/main/java/com/calculator/Calculator.java
+++ src/main/java/com/calculator/Calculator.java
@@ -2,6 +2,7 @@ package com.calculator;
 
 public class Calculator {
 	public static void main(String[] args){
+		System.out.println("hello world");
 		System.out.println("6 + 3 = " + add(6, 3));
 		System.out.println("6 - 3 = " + subtract(6,3));
 		System.out.println("6 * 3 = " + multiply(6,3));
